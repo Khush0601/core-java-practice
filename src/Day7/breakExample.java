669a66ae5b141980162cebbd84a59fe3c4821f15
Khushboo Kumari <133033913+Khush0601@users.noreply.github.com>
2026-01-19 13:08:24 +0530

    break and continue practiced

---
+++ src/Day7/breakExample.java
@@ -0,0 +1,14 @@
+package Day7;
+
+public class breakExample {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
---
+++ src/Day7/continueExample.java
@@ -0,0 +1,14 @@
+package Day7;
+
+public class continueExample {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
